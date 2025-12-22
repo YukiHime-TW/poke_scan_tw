64d012b712cc522b3ea72085a7b0b714c37c47e7
Daniel.Yeh <36097203+YukiHime-TW@users.noreply.github.com>
2025-12-22 13:15:35 +0800

    更新資料庫結構，修改讀取方式

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3934951011"
+    serviceWorkerVersion: "3557032788"
   }
 });
