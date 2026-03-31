847c5bacbf28cadb32300904020e2b61f905bc50
Daniel.Yeh <36097203+YukiHime-TW@users.noreply.github.com>
2026-03-31 16:28:39 +0800

    更新資料庫

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3616462771"
+    serviceWorkerVersion: "2568450346"
   }
 });
